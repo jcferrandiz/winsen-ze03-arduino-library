3faa07f395417f795a0eaa4d780626e840b846d1
Juan Carlos Ferrándiz Pardo <jferrandiz@rhomb.io>
2021-06-22 13:48:27 +0200

    Sensor library modified

--- src/WinsenZE03.cpp
+++ src/WinsenZE03.cpp
@@ -18,48 +18,81 @@ void WinsenZE03::begin(Stream *ser, int type){
 }
 
 void WinsenZE03::setAs(bool active){
-  byte setConfig[] = {0xFF, 0x01, 0x78, 0x04, 0x00, 0x00, 0x00, 0x00, 0x83};//QA config
+  byte setConfig[] = {0xFF, 0x01, 0x78, 0x41, 0x00, 0x00, 0x00, 0x00, 0x46};//QA config
   byte response[9] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
   if (active){
-    setConfig[3] =0x03;
-    setConfig[8] =0x84;
+    setConfig[3] =0x40;
+    setConfig[8] =0x47;
   }
   _s->write(setConfig,sizeof(setConfig));
   // Wait for the response
   delay(2000);
-  //Flush the incomming buffer
+  //Flush the incoming buffer
   if (_s->available() > 0) {
     _s->readBytes(response,9);
   }
+
+  SerialUSB.println("Response: ");
+  debugPrint(response);
+
   while(_s->available()>0){
     byte c = _s->read();
   }
 }
 
 float WinsenZE03::readContinuous(){
+  float ppm = 0;
+
   if (_s->available() > 0) {
-    byte measure[8];
-    _s->readBytes(measure,9);
-    // incomingByte = _s.read();
-    float ppm = measure[2]*256+measure[3];
-    return ppm;
+    byte measure[9];
+
+    while (_s->available() > 0) {
+      _s->readBytes(measure, 9);
+      // incomingByte = _s.read();
+    }
+
+    if (DEVMODE) {
+      SerialUSB.println("Measure: ");
+      debugPrint(measure);
+      ppm = measure[4] * 256 + measure[5];
+    }
+  } else if (DEVMODE) {
+    SerialUSB.println("No data in buffer.");
   }
+  return ppm;
 }
 
+//This function is not working right now
 float WinsenZE03::readManual(){
   float ppm;
-  byte petition[] = {0xFF,0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};// Petition to get a single result
-  byte measure[8]={0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};// Space for the response
+  //Modify petition format depending on the sensor
+  byte petition[] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};// Petition to get a single result
+  byte measure[9] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};  // Space for the response
   _s->write(petition,sizeof(petition));
+  
+  //TO DO: Modify this delay and use a callback in this function
   delay(1500);
   // read
   if (_s->available() > 0) {
-    _s->readBytes(measure,9);
+    byte measure[9];
+
+    while (_s->available() > 0) {
+      _s->readBytes(measure, 9);
+      // incomingByte = _s.read();
+    }
+    if(DEVMODE){
+      SerialUSB.println("Measure: ");
+      debugPrint(measure);
+      ppm = measure[4] * 256 + measure[5];
+    }
+  } else if (DEVMODE) {
+    SerialUSB.println("No data in buffer.");
   }
+
   // calculate
-  if (measure[0]==0xff && measure[1]==0x86){
-    ppm = measure[2]*256+measure[3];// this formula depends of the sensor is in the dataSheet
+  if (measure[0]==0xff && measure[1]==0x2A){
+    ppm = measure[4]*256+measure[5];// this formula depends of the sensor is in the dataSheet
     if (_type == 2){
       ppm = ppm*0.1;
     }
@@ -70,13 +103,21 @@ float WinsenZE03::readManual(){
 }
 
 void WinsenZE03::debugPrint(byte arr[]){
-  Serial.print(arr[0],HEX);Serial.print(" ");
-  Serial.print(arr[1],HEX);Serial.print(" ");
-  Serial.print(arr[2],HEX);Serial.print(" ");
-  Serial.print(arr[3],HEX);Serial.print(" ");
-  Serial.print(arr[4],HEX);Serial.print(" ");
-  Serial.print(arr[5],HEX);Serial.print(" ");
-  Serial.print(arr[6],HEX);Serial.print(" ");
-  Serial.print(arr[7],HEX);Serial.print(" ");
-  Serial.println(arr[8],HEX);
+  SerialUSB.print(arr[0], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[1], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[2], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[3], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[4], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[5], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[6], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.print(arr[7], HEX);
+  SerialUSB.print(" ");
+  SerialUSB.println(arr[8], HEX);
 }
